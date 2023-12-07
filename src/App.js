fd9d978aaec397a3a9e86e37218f61658342b214
shahZaheed <zahidshah99599@gmail.com>
2023-12-07 15:13:52 +0530

    no changes

--- src/App.js
+++ src/App.js
@@ -6,13 +6,6 @@ import productSlice from './product/productSlice';
 import { useSelector } from 'react-redux';
 
 function App() {
-  const initialState = {
-    items: [
-      { id: 1, name: 'Product 1', price: 10 },
-      { id: 2, name: 'Product 2', price: 20 },
-      { id: 3, name: 'Product 3', price: 30 },
-    ],
-  };
   const initialBasketItems = useSelector(state => state.product.items)
   const basketItems = useSelector(state => state.basket.items)
   console.log("basketItems" + initialBasketItems);
